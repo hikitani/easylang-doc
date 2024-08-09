fc62117ff6ac3f2338f279a964b9452d6d1dde81
hikitani <kazbekmamakaev@mail.ru>
2024-08-09 22:37:36 +0300

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+  logo: <span>Easylang</span>,
+  project: {
+    link: "https://github.com/hikitani/easylang",
+  },
+  primaryHue: 160,
+  primarySaturation: 50,
+  i18n: [
+    { locale: "ru-RU", text: "Русский" },
+    { locale: "en-US", text: "English" },
+  ],
+};
